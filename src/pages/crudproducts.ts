3b7371da7de038edd70b5d16e1fa34cc7270dd49
Bayrpm <bayron_snbk_7@hotmail.com>
2024-06-17 21:21:35 -0400

    a
    s

---
+++ src/pages/crudproducts.ts
@@ -0,0 +1,61 @@
+import { supabase } from "../db/supabase";
+
+export async function getProducts() {
+    const { data, error } = await supabase
+        .from('producto')
+        .select(`
+            idproducto,
+            nombre,
+            precio,
+            descripcion,
+            imagen,
+            stock,
+            categoria ( idcategoria, nombre ),
+            opcion ( idopcion, nombre ),
+            sucursal ( idsucursal, nombre )
+        `);
+
+    if (error) {
+        console.error('Error fetching products:', error);
+        return [];
+    }
+    return data;
+}
+
+export async function addProduct(product: { nombre: string; precio: string; descripcion: string; imagen: string; stock: string; idcategoria: string; idopcion: string; idsucursal: string; }) {
+    const { data, error } = await supabase
+        .from('producto')
+        .insert([product]);
+
+    if (error) {
+        console.error('Error adding product:', error);
+        return null;
+    }
+    return data;
+}
+
+export async function updateProduct(idproducto: any, updatedProduct: {}) {
+    const { data, error } = await supabase
+        .from('producto')
+        .update(updatedProduct)
+        .eq('idproducto', idproducto);
+
+    if (error) {
+        console.error('Error updating product:', error);
+        return null;
+    }
+    return data;
+}
+
+export async function deleteProduct(idproducto: any) {
+    const { data, error } = await supabase
+        .from('producto')
+        .delete()
+        .eq('idproducto', idproducto);
+
+    if (error) {
+        console.error('Error deleting product:', error);
+        return null;
+    }
+    return data;
+}
